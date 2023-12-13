1f7b10186f8d3cd10464307cc01ae159678f0d8f
layeska <lisa13suarez@gmail.com>
2023-12-12 23:22:05 -0600

    añadiendo conexión typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'ana1234',
+      database: 'postgres',
+      synchronize: true, // Solo debería usarse en desarrollo, desactivar en producción
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
